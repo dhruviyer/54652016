6d11dec5f2b0b18db1b2f4674a2e223f7824b2f4
SachinKonan <sachinkonan480@gmail.com>
2016-11-05 10:29:52 -0700

    udp code

---
+++ 5465 2016/src/org/usfirst/frc/team5465/robot.v4.oct8,2016/udpshit/UDPThread.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team5465.robot.UDPSTuff;
+
+import java.io.*;
+import java.net.*;
+
+public class UDPThread extends Thread
+{
+	private String name;
+	private DatagramSocket serverSocket;
+	private byte[] receiveData;
+	private String receiveSentence;
+	private boolean interrupt;
+	public UDPThread(String name1, int port, int buffersize) 
+	{
+		super(name1);
+		
+		try 
+		{
+			serverSocket = new DatagramSocket(port);
+			System.out.println("Starting Server");
+		} 
+		catch (SocketException e)
+		{
+			System.out.println("UDP Server can't be made :( ");
+		}
+		receiveData = new byte[buffersize];
+		receiveSentence = "";
+		interrupt = false;
+	}
+	
+	public void run()
+	{
+		System.out.println("Starting Thread");
+		while(!interrupt)
+        {
+		
+           DatagramPacket receivePacket = new DatagramPacket(receiveData, 0,receiveData.length);
+          
+           try 
+           {
+        	   serverSocket.receive(receivePacket);
+           } 
+           catch (IOException e) 
+           {
+        	   System.out.println("hello boiii");
+        	   e.printStackTrace();
+           }
+           
+           receiveSentence = new String(receivePacket.getData());
+          
+        }
+	}
+	
+	public String getString()
+	{
+		return receiveSentence;
+	}
+	
+	public void updateInterrupt()
+	{
+		interrupt = true;
+	}
+
+}
